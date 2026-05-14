d95b6b5fb779ee08b69460851ba59d4a267271ec
alexa <akolp@csd.auth.gr>
2026-05-14 20:22:53 +0300

    Εμφάνιση όλων των βιβλίων εξαρχής στο items.html

--- ergasia/js/items.js
+++ ergasia/js/items.js
@@ -23,6 +23,71 @@ function dislikeClickedItems(){
 }
 
 let booksDisplayedIds = {};
+window.addEventListener("load", async function displayAllItems() {
+    const url = `http://127.0.0.1:5000/search`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        const grid = document.getElementById("grid-items");
+        grid.innerHTML = "";
+        booksDisplayedIds = {};
+
+        for (let i = 0; i < data.length; i++) {
+            let result = document.createElement("div");
+            result.setAttribute("class", "result");
+            grid.appendChild(result);
+
+            let resultImage = document.createElement("img")
+            resultImage.setAttribute("src", data[i].image);
+            resultImage.setAttribute("style", "width: 100%");
+            resultImage.setAttribute("alt", "Το εξώφυλλο του " + "\"" + data[i].name + "\"");
+            result.appendChild(resultImage)
+
+            let captionText = document.createElement("div");
+            captionText.setAttribute("class", "caption-text");
+            result.appendChild(captionText);
+            let bookTitle = document.createElement("div");
+            bookTitle.setAttribute("class", "book-title");
+            bookTitle.innerText = data[i].name;
+            captionText.appendChild(bookTitle);
+
+            let description = document.createElement("div");
+            description.setAttribute("class","description");
+            description.innerText = data[i].description;
+            captionText.appendChild(description);
+
+            let priceDislike = document.createElement("div");
+            priceDislike.setAttribute("class", "price-dislike");
+            result.appendChild(priceDislike);
+            let price = document.createElement("div");
+            price.setAttribute("class", "price");
+            price.innerText = data[i].price + "$";
+            priceDislike.appendChild(price);
+            let dislikeSection = document.createElement("div");
+            dislikeSection.setAttribute("class", "dislike-section");
+            let button = document.createElement("button");
+            button.setAttribute("id", "book" + (i + 1));
+            dislikeSection.appendChild(button);
+            let buttonImage = document.createElement("img");
+            buttonImage.setAttribute("src", "../ergasia/img/dislike-before.png");
+            buttonImage.setAttribute("alt", "κουμπί dislike");
+            button.appendChild(buttonImage);
+            dislikeSection.innerHTML += data[i].likes;
+            priceDislike.appendChild(dislikeSection);
+
+            booksDisplayedIds["book"+(i+1)] = data[i].id;
+        }
+        dislikeStartItems();
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
 document.getElementById("search-form").addEventListener("submit", async function search(event) {
     event.preventDefault();
     const input = document.getElementById("search-input").value;
